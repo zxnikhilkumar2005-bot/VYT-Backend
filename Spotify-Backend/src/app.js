39a700b1167b16ee99ec29ba50f5c361041f07b6
Nikhil kumar <zxnihilkumar2005@gmail.com>
2026-03-30 14:52:57 +0530

    Initialize Spotify backend with essential files and dependencies
    - Created package.json with initial dependencies including express, mongoose, and dotenv.
    - Added server.js as the entry point for the application.
    - Implemented app.js to set up express server and middleware for JSON parsing and cookie handling.

---
+++ Spotify-Backend/src/app.js
@@ -0,0 +1,11 @@
+const express = require ('express');
+const cookieParser = require('cookie-parser');
+
+
+const app = express();
+aap.use(express.json());
+app.use(cookieParser());
+
+
+
+module.exports =app
